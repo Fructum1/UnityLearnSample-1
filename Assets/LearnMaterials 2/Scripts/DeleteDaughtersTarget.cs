ba15d34197b691307c422267fcaf8f5c33175e5f
Fructum1 <gaglu666@gmail.com>
2022-04-08 14:19:02 +0700

    TS-update

--- Assets/LearnMaterials 2/Scripts/DeleteDaughtersTarget.cs
+++ Assets/LearnMaterials 2/Scripts/DeleteDaughtersTarget.cs
@@ -22,20 +22,32 @@ public class DeleteDaughtersTarget : SampleScript
     // Start is called before the first frame update
     void Start()
     {
-        Vector3 s = new Vector3(0.01F, 0.01F, 0.01F);
-        foreach (Transform child in target)
-        {
-            while (child.localScale != s)
-            {
-                child.localScale -= new Vector3(0.01F, 0.01F, 0.01F);
-            }
-            //GameObject.Destroy(child.gameObject);
-        }
+        //Vector3 s = new Vector3(0.01F, 0.01F);
+        //float minimum = 0.01F;
+        //float maximum = 0.01F;
+
+        //foreach (Transform child in target)
+        //{
+        //    //while (child.localScale != s)
+        //    //{
+        //        child.localScale += new Vector3(Mathf.Lerp(minimum, maximum, Time.time), Mathf.Lerp(minimum, maximum, Time.time), Mathf.Lerp(minimum, maximum, Time.time));
+        //    //}
+        //    //GameObject.Destroy(child.gameObject);
+        //}
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        float minimum = 0.001F;
+        float maximum = 0.0001F;
+
+        foreach (Transform child in target)
+        {
+            while (child.localScale.x >= 0)
+            {
+                child.localScale -= new Vector3(Mathf.Lerp(minimum, maximum, Time.time), Mathf.Lerp(minimum, maximum, Time.time), Mathf.Lerp(minimum, maximum, Time.time));
+            }        
+        }
     }
 }
